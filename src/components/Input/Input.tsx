45910e5904d92ecf5039d6880906ee8670016548
AnnMoiseieva <anamoiseieva@gmail.com>
2025-03-26 06:18:37 +0100

    add homework 6 dd 25.03.25 - component Input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,9 @@
+import "./style.css";
+
+function Input() {
+  return <div>
+            <input className="input" type="text" placeholder="Enter your name..." />
+        </div>
+}
+
+export default Input
