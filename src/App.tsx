7535c1978c6d7f04c0f00aa867998cda4cc5da4c
AnnMoiseieva <anamoiseieva@gmail.com>
2025-04-01 10:17:39 +0200

    add homework 10 dd 31.03.2025

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,15 @@ import GlobalStyles from "./styles/GlobalStyles";
 // import Lesson07 from "./lessons/Lesson07/Lesson07";
 // import Lesson08 from "./lessons/Lesson08/Lesson08";
 // import Lesson09 from "./lessons/Lesson09/Lesson09";
-import Lesson10 from "./lessons/Lesson10/Lesson10";
+// import Lesson10 from "./lessons/Lesson10/Lesson10";
+
 
 // Homeworks imports:
 // import Homework06 from "./homeworks/Homework06/Homework06";
 // import Homework07 from "./homeworks/Homework07/Homework07";
 // import Homework08 from "./homeworks/Homework08/Homework08";
 // import Homework09 from "./homeworks/Homework09/Homework09";
-
+import Homework10 from "./homeworks/Homework10/Homework10";
 
 // Consultations imports:
 // import Consultation04 from "./consultations/Consultation04/Consultation04";
@@ -31,12 +32,13 @@ function App() {
       {/* <Lesson07 /> */}
       {/* <Lesson08 /> */}
       {/* <Lesson09 /> */}
-      <Lesson10 />
+      {/* <Lesson10 /> */}
 
       {/* <Homework06 /> */}
       {/* <Homework07 /> */}
       {/* <Homework08 /> */}
       {/* <Homework09 /> */}
+      <Homework10/>
 
       {/* <Consultation04 /> */}
     </>
---
+++ src/homeworks/Homework10/styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Homework10Container = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+  flex: 1;
+  padding: 60px;
+  background-image: linear-gradient(to right, rgb(1, 1, 19), rgb(132, 165, 231))
+`;
