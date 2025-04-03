b341d4e5ff1b67c010a4bba8003879c944ce82bd
AnnMoiseieva <anamoiseieva@gmail.com>
2025-04-03 13:04:58 +0200

    add Lesson 13 + Homewowk 13 dd 03.04.2025

---
+++ src/lessons/Lesson13/Lesson13.tsx
@@ -0,0 +1,90 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import Button from "../../components/Button/Button";
+import {
+  ImageWrapper,
+  Lesson13Container,
+  Error,
+  ImageContainer,
+  ButtonInputContainer,
+} from "./styles";
+import Spinner from "../../components/Spinner/Spinner";
+import Input from "../../components/Input/Input";
+
+function Lesson13() {
+  // const [image, setImage] = useState<string | undefined>(undefined);
+  const [images, setImages] = useState<string[] | undefined[]>([]);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const IMAGE_URL: string = "https://dog.ceo/api/breeds/image/random";
+  const getImage = async () => {
+    setError(undefined);
+    try {
+      setIsLoading(true);
+      const response = await axios.get(IMAGE_URL);
+      //   console.log(response.data);
+
+      setImages((prevImages) => [...prevImages, response.data.message]);
+    } catch (error: any) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getImage();
+  }, []);
+
+  const handleInputChange = async (
+    element: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setInputValue(element.target.value);
+    if (element.target.value) getImage();
+    // console.log(element);
+  };
+
+  const deleteAllData = () => {
+    setImages([]);
+  };
+
+  return (
+    <Lesson13Container>
+      <ButtonInputContainer>
+        <Button
+          name="GET MORE IMAGES"
+          onClick={getImage}
+          disabled={isLoading}
+        />
+        <Input
+          name=""
+          placeholder="Enter your request"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        {images.length > 0 && (
+          <Button name="DELETE ALL DATA" onClick={deleteAllData} />
+        )}
+      </ButtonInputContainer>
+
+      <Error>{error}</Error>
+
+      <ImageContainer>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          images.map((image, index) => (
+            <ImageWrapper>
+              <img src={image} alt="Animal" />
+            </ImageWrapper>
+          ))
+        )}
+      </ImageContainer>
+    </Lesson13Container>
+  );
+}
+
+export default Lesson13;
---
+++ src/lessons/Lesson13/styles.ts
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+
+export const Lesson13Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+  flex: 1;
+  /* padding: 60px; */
+  background-image: linear-gradient(
+    to right,
+    rgb(2, 44, 55),
+    rgb(173, 172, 159)
+  );
+`;
+
+export const ButtonInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-width: 500px;
+  min-height: 400px;
+  /* width: 100%; */
+  /* background-color: white; */
+  border-radius: 10px;
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.5);
+`;
+
+export const ImageWrapper = styled.div`
+  margin: 16px;
+  background-color: white;
+  max-height: 700px;
+  padding: 30px;
+  overflow-y: auto;
+  max-width: 100%;
+  overflow-x: auto;
+`;
+
+export const Error = styled.div`
+  font-size: 28px;
+  color: red;
+  font-weight: bold;
+`;
